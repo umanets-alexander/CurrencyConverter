ae3547678080a3079658cde9f448fdb0bc013006
Alexandr <umanets_alexander@mail.ru>
2021-06-29 11:24:26 +0700

    Реализация связи и подключения с JSON

---
+++ JSONConnect.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    public class JSONConnect
+    {
+        public static JSONConvert.ConverterValute DataCurrency;
+
+        public static void connectJSON()
+        {
+            WebClient client = new WebClient();
+            string stringJSON = client.DownloadString("https://www.cbr-xml-daily.ru/daily_json.js");
+            DataCurrency = JsonConvert.DeserializeObject<JSONConvert.ConverterValute>(stringJSON);
+        }
+    }
+}
---
+++ JSONConvert.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    public class JSONConvert
+    {
+        public class AUD
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class AZN
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class GBP
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class AMD
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class BYN
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class BGN
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class BRL
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class HUF
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class HKD
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class DKK
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class USD
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class EUR
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class INR
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class KZT
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class CAD
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class KGS
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class CNY
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class MDL
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class NOK
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class PLN
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class RON
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class XDR
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class SGD
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class TJS
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class TRY
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class TMT
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class UZS
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class UAH
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class CZK
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class SEK
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class CHF
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class ZAR
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class KRW
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class JPY
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public double Previous { get; set; }
+        }
+
+        public class Valute
+        {
+            public AUD AUD { get; set; }
+            public AZN AZN { get; set; }
+            public GBP GBP { get; set; }
+            public AMD AMD { get; set; }
+            public BYN BYN { get; set; }
+            public BGN BGN { get; set; }
+            public BRL BRL { get; set; }
+            public HUF HUF { get; set; }
+            public HKD HKD { get; set; }
+            public DKK DKK { get; set; }
+            public USD USD { get; set; }
+            public EUR EUR { get; set; }
+            public INR INR { get; set; }
+            public KZT KZT { get; set; }
+            public CAD CAD { get; set; }
+            public KGS KGS { get; set; }
+            public CNY CNY { get; set; }
+            public MDL MDL { get; set; }
+            public NOK NOK { get; set; }
+            public PLN PLN { get; set; }
+            public RON RON { get; set; }
+            public XDR XDR { get; set; }
+            public SGD SGD { get; set; }
+            public TJS TJS { get; set; }
+            public TRY TRY { get; set; }
+            public TMT TMT { get; set; }
+            public UZS UZS { get; set; }
+            public UAH UAH { get; set; }
+            public CZK CZK { get; set; }
+            public SEK SEK { get; set; }
+            public CHF CHF { get; set; }
+            public ZAR ZAR { get; set; }
+            public KRW KRW { get; set; }
+            public JPY JPY { get; set; }
+        }
+
+        public class ConverterValute
+        {
+            public DateTime Date { get; set; }
+            public DateTime PreviousDate { get; set; }
+            public string PreviousURL { get; set; }
+            public DateTime Timestamp { get; set; }
+            public Valute Valute { get; set; }
+        }
+    }
+}
