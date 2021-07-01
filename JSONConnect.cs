f6a0b7bd61d649c11b76d5c67cc3bcf61a34950c
Alexandr <umanets_alexander@mail.ru>
2021-07-01 11:55:42 +0700

    Добавлена кодировка в utf8

--- JSONConnect.cs
+++ JSONConnect.cs
@@ -15,6 +15,7 @@ namespace CurrencyConverter
         public static void connectJSON()
         {
             WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
             string stringJSON = client.DownloadString("https://www.cbr-xml-daily.ru/daily_json.js");
             DataCurrency = JsonConvert.DeserializeObject<JSONConvert.ConverterValute>(stringJSON);
         }
